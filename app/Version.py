de6177d5095c84eea413a25d45c3d9fd44f42e29
giwon.kang <giwon.kang@lge.com>
2023-03-02 08:52:23 +0900

    Update Version.py

--- app/Version.py
+++ app/Version.py
@@ -1,4 +1,35 @@
+"""
+Version
+"""
 
+#
+# change if can't be compatible with previoud version
+#
+Major = 1
+
+#
+# change if new function is added
+#
+Minor = 0
+
+#
+# increase if bug is fixed
+#
+Patch = 0
 
 def getVersion():
-    return 'Ver1.0.0'
+    version = f'Ver{Major}.{Minor}.{Patch}'
+    return version
+
+
+
+"""
+Test 
+"""
+
+def testVersion():
+    print('*** print version ***')
+    print(getVersion())
+
+if __name__ == "__main__":
+    testVersion()
