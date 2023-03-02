1be8ec0b71d19a479f92be3e4240ccc532a21213
giwon.kang <giwon.kang@lge.com>
2023-03-02 10:38:06 +0900

    Update KorDivStockAnalyzerThread.py

--- app/KorDivStockAnalyzerThread.py
+++ app/KorDivStockAnalyzerThread.py
@@ -6,6 +6,9 @@ Author
 - https://blog.naver.com/shawgibal
 """
 
+import os
+import sys
+
 from tkinter import *            # tkinter 라이브러리에 모든 함수를 사용
 import tkinter.ttk
 
@@ -24,6 +27,14 @@ from AppLogger import LOG
 from KorDivStockAnalyzer import KorDivStockAnalyzer
 import Config
 import AppUtil
+
+
+kodivstock_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
++ '/kodivstock/')
+sys.path.append(kodivstock_dir)
+import divexcelanalyzer as dea
+
+
 
 """
 KorDivStockAnalyzerThread
@@ -46,6 +57,21 @@ class KorDivStockAnalyzerThread(threading.Thread):
 
         # 주가와 배당금 데이터 수집, 분석 
         try:
+            stockCode = self.root.stockCode
+
+            # check whether code is in the TIGER ETF 50 or not
+            divExcelAnalyzer = dea.DivExcelAnalyzer('./data/tiger_divgrowth50_dps_data_2012_2021.xlsx')
+            tickers = divExcelAnalyzer.getTickers()
+            if Config.isRelease() and not stockCode in tickers:
+                message = 'TIGER 배당성장 ETF 보유 종목이 아닙니다.'
+                self.root.statusLabel.configure(text = message)
+                self.root.threadCb(False)
+                return
+
+            divGrowthYear = divExcelAnalyzer.getDivGrowthYears(stockCode)
+            divGrowthRate3 = divExcelAnalyzer.getDivGrowthRates(stockCode, 3)
+            divGrowthRate7 = divExcelAnalyzer.getDivGrowthRates(stockCode, 7)
+
             stockName = self.stockAnalyzer.getStockName()
 
             self.root.statusLabel.configure(text = f'{stockName} 데이터를 수집합니다.')
