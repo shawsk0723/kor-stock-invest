b91e2eb9b6e6f2a06ba573932f68e2615400145d
giwon.kang <giwon.kang@lge.com>
2023-03-02 13:50:48 +0900

    Create DivHistoryAnalyzer.py

---
+++ app/DivHistoryAnalyzer.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from AppLogger import LOG
+
+kodivstock_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
++ '/kodivstock/')
+sys.path.append(kodivstock_dir)
+import divexcelanalyzer as dea
+
+
+class DivHistoryAnalyzer:
+    def __init__(self):
+        self.divExcelAnalyzer = dea.DivExcelAnalyzer('./data/tiger_divgrowth50_dps_data_2012_2021.xlsx')
+
+    def checkStockCodeInTigerETF(self, stockCode):
+        tickers = self.divExcelAnalyzer.getTickers()
+        if not stockCode in tickers:
+            raise(Exception(f'<{stockCode}> TIGER 배당성장 ETF 보유 종목이 아닙니다.'))
+
+    def getDivGrowthInfo(self, stockCode):
+        divGrowthInfo = {}
+        divGrowthYears = self.divExcelAnalyzer.getDivGrowthYears([stockCode])
+        LOG(f'div growth years {divGrowthYears}')
+        divGrowthInfo['배당 성장 연수'] = divGrowthYears
+
+        divGrowthRate3 = self.divExcelAnalyzer.getDivGrowthRates([stockCode], 3)
+        LOG(f'3 year div growth rates  {divGrowthRate3}')
+        divGrowthInfo['배당 성장률(3)'] = divGrowthRate3
+
+        divGrowthRate7 = self.divExcelAnalyzer.getDivGrowthRates([stockCode], 7)
+        LOG(f'7 year div growth rates  {divGrowthRate7}')
+        divGrowthInfo['배당 성장률(7)'] = divGrowthRate7
