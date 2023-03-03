650fbc22a594a25871e1ed4d62914dc08a0bd284
giwon.kang <giwon.kang@lge.com>
2023-03-03 13:56:53 +0900

    Update KorDivStockAnalyzerThread.py

--- app/KorDivStockAnalyzerThread.py
+++ app/KorDivStockAnalyzerThread.py
@@ -38,14 +38,19 @@ class KorDivStockAnalyzerThread(threading.Thread):
     def __init__(self, root):
         threading.Thread.__init__(self)
         self.root = root
-        self.divHistoryAnalyzer = DivHistoryAnalyzer()
-        self.stockPricer = KorDivStockPricer(self.root.stockCode)
+        self.divHistoryAnalyzer = None
+        self.stockPricer = None
+        self.stockName = ""
+        self.analysisResult = {}
 
     def run(self):
         LOG('KorDivStockAnalyzerThread Start...')
 
         # output 폴더가 없다면 새로 생성
         AppUtil.makeDirIfNotExist(Config.OUR_DIR)
+
+        self.divHistoryAnalyzer = DivHistoryAnalyzer()
+        self.stockPricer = KorDivStockPricer(self.root.stockCode)
 
         # GUI 업데이트
         self.root.startButton['state'] = DISABLED
@@ -57,24 +62,32 @@ class KorDivStockAnalyzerThread(threading.Thread):
             stockCode = self.root.stockCode
 
             # check whether code is in the TIGER ETF 50 or not
+            self.root.statusText.insert(END, f'{stockCode} -> 주식 코드를 확인합니다.\n')
             if Config.isRelease():
                 self.divHistoryAnalyzer.checkStockCodeInTigerETF(stockCode)
 
+            # get stock name
+            self.stockName = self.stockPricer.getStockName()
+            self.root.statusText.insert(END, f'주식 이름은 <<{self.stockName}>>입니다.\n')
+
+            self.root.statusText.insert(END, f'배당 성장 정보를 확인합니다.\n')
             self.divGrowthInfo = self.divHistoryAnalyzer.getDivGrowthInfo(stockCode)
 
-            # get stock name
-            stockName = self.stockPricer.getStockName()
+            """ TEST GUI S """
+            #self.root.threadCb(True)
+            #return
+            """ TEST GUI E """
 
             # collect stock data
-            self.root.statusText.insert(END, f'{stockName} 데이터를 수집합니다.')
-
+            self.root.statusText.insert(END, f'주가/배당금 데이터를 수집합니다.\n')
             self.stockPricer.collectStockData()
 
             # analyze stock data
-            self.root.statusText.insert(END, f'{stockName} 데이터를 분석합니다.')
+            self.root.statusText.insert(END, f'주가/배당금 데이터를 분석합니다.\n')
             self.pricingResult = self.stockPricer.doStockPricing()
 
-            self.root.statusText.insert(END, f'{stockName} 데이터 분석을 완료하였습니다.')
+            self.analysisResult = {**self.pricingResult, **self.divGrowthInfo}
+            self.root.statusText.insert(END, f'데이터 분석을 완료하였습니다.\n')
 
             self.root.threadCb(True)
         except Exception as e:
@@ -86,4 +99,12 @@ class KorDivStockAnalyzerThread(threading.Thread):
             self.root.progressbar.stop()
             LOG('KorDivStockAnalyzerThread Stop...')
 
+    def getStockName(self):
+        return self.stockName
 
+    def getAnalysisResult(self):
+        return self.analysisResult
+
+    def saveResult(self):
+        self.stockPricer.savePriceDivChart()
+        # To-Do save analysis result to excel file
