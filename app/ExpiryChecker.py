935da4d5497890fc1ec0a22d2abde2eb916b16d8
giwon.kang <giwon.kang@lge.com>
2023-03-03 15:55:01 +0900

    Update ExpiryChecker.py

--- app/ExpiryChecker.py
+++ app/ExpiryChecker.py
@@ -1,7 +1,29 @@
+"""
+KorDivStockAnalyzerApp
+
+Author
+- 코드장인
+- https://blog.naver.com/shawgibal
+"""
+
 from datetime import datetime
-#from datetime import timedelta
+from datetime import timedelta
 import Config
 
 def checkExpiry():
     expire_date = Config.APP_EXPIRE_DATE
     return datetime.today() > expire_date
+
+def getExiryDate():
+    exiryDate = (Config.APP_EXPIRE_DATE - timedelta(days=1)).strftime("%Y-%m-%d")
+    return exiryDate
+
+"""
+TEST
+"""
+if __name__ == '__main__':
+    expiryDate = getExiryDate()
+    print(f'Expiry Date = {expiryDate}')
+
+    checkExpiryResult = checkExpiry()
+    print(f'Expiry Check Result = {checkExpiryResult}')
