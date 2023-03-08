4aa03a6a0007602f171c6c88920ca435711d445d
shawsk0723 <shawsk0723@gmail.com>
2023-03-08 17:44:53 +0900

    Update UserSettings.py

--- app/UserSettings.py
+++ app/UserSettings.py
@@ -8,13 +8,64 @@ Author
 
 import pandas as pd
 
+
+class StockLister:
+    def __init__(self, filePath, stockCodeKey):
+        self.filePath = filePath
+        self.stockCodeKey = stockCodeKey
+
+    def getFirstStockCode(self):
+        try:
+            df = pd.read_csv(self.filePath)
+            stockCodeKey = self.stockCodeKey
+            return str(df[stockCodeKey][0]).zfill(6)
+        except Exception as e:
+            print(str(e))
+            return ""
+
+    def getStockCodeList(self):
+        try:
+            df = pd.read_csv(self.filePath)
+            stockCodeKey = self.stockCodeKey
+            stockCodes = []
+            for stockCode in df[stockCodeKey]:
+                stockCodes.append(str(stockCode).zfill(6))
+            return stockCodes
+        except Exception as e:
+            print(str(e))
+            return ""
+
+
 def getDefaultStockCode():
     try:
         df = pd.read_csv('./data/stocklist.csv')
-        return str(df.code[0]).zfill(6)
+        return str(df.StockCode[0]).zfill(6)
     except Exception as e:
+        print(str(e))
+        return ""
+
+def getStockCodeList():
+    try:
+        df = pd.read_csv('./data/stocklist.csv')
+        stockCodes = []
+        for stockCode in df.StockCode:
+            stockCodes.append(str(stockCode).zfill(6))
+        return stockCodes
+    except Exception as e:
+        print(str(e))
         return ""
 
 
 if __name__ == '__main__':
-  print(getDefaultStockCode())
+    stockLister = StockLister('./data/stocklist.csv', 'StockCode')
+    defaultStockCode = stockLister.getFirstStockCode()
+    print(f'defaultStockCode = {defaultStockCode}')
+    stockCodeList = stockLister.getStockCodeList()
+    print(f'stockCodeList = {stockCodeList}')
+
+    """
+    defaultStockCode = getDefaultStockCode()
+    print(f'defaultStockCode = {defaultStockCode}')
+    stockCodeList = getStockCodeList()
+    print(f'stockCodeList = {stockCodeList}')
+    """
