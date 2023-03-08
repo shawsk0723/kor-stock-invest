3d431c7037008dad5da15cab71277e7d0a2bbb1e
giwon.kang <giwon.kang@lge.com>
2023-03-09 08:20:47 +0900

    Update StockUtil.py

--- app/StockUtil.py
+++ app/StockUtil.py
@@ -9,17 +9,8 @@ Author
 from datetime import datetime
 from datetime import timedelta
 
-import os
-import numpy as np
-import pandas as pd
-from scipy.signal import savgol_filter
+
 from pykrx import stock
-import matplotlib.pyplot as plt
-import time
-#plt.rcParams['font.family'] = 'Malgun Gothic'
-
-from AppLogger import LOG
-
 
 
 def getLastBusinessDay():
