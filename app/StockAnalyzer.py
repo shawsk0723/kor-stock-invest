13a2b3eb08669df7866dc7ed91a6dfa43be210c2
shawsk0723 <shawsk0723@gmail.com>
2023-03-11 21:00:30 +0900

    Create StockAnalyzer.py

---
+++ app/StockAnalyzer.py
@@ -0,0 +1,25 @@
+"""
+StockAnalyzer
+
+Author
+- 코드장인
+- https://blog.naver.com/shawgibal
+"""
+
+
+class StockAnalyzer:
+    def __init__(self):
+        self.stockNameList = []
+        self.analysisResult = {}
+
+    def execute(self):
+        pass
+
+    def getStockName(self):
+        return self.stockNameList
+
+    def getAnalysisResult(self):
+        return self.analysisResult
+
+    def saveResult(self):
+        pass
